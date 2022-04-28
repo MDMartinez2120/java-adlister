f36fc0ee4c8d6bf5f6b427647f7004b2e71a3e82
Matthew Martinez <matt.martinez2120@gmail.com>
2022-04-28 11:49:14 -0500

    finished the first three steps of the beans exercises.

---
+++ src/main/java/Author.java
@@ -0,0 +1,24 @@
+import java.io.Serializable;
+
+public class Author implements Serializable {
+    private int id;
+    private String author_name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAuthor_name() {
+        return author_name;
+    }
+
+    public void setAuthor_name(String author_name) {
+        this.author_name = author_name;
+    }
+
+    public Author(){}
+}
