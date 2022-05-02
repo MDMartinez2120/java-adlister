27bf2485870b32cdc40ed6bd2c61122332834f6e
Matthew Martinez <matt.martinez2120@gmail.com>
2022-05-02 12:19:44 -0500

    finished the sessions and cookies exercise.

--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -3,11 +3,34 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
+
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+       if (request.getSession().getAttribute("user") == null) {
+           response.sendRedirect("/login");
+           return;
+       }
+
         request.getRequestDispatcher("/profile.jsp").forward(request, response);
     }
-}
+
+};
+
+//    HttpSession session = req.getSession();
+//
+//        if (req.getParameter("forget") != null) {
+//                session.removeAttribute("font-color");
+//                session.removeAttribute("bg-color");
+//                resp.sendRedirect("/color-profile");
+//                } else {
+//                session.setAttribute("font-color", req.getParameter("font-color"));
+//                session.setAttribute("bg-color", req.getParameter("bg-color"));
+//                // redirect user to the "/ads"
+//                resp.sendRedirect("/ads");
+//                }
+//                }
--- src/main/webapp/profile.jsp
+++ src/main/webapp/profile.jsp
@@ -1,16 +1,22 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
+    <title>Profile</title>
     <jsp:include page="partials/head.jsp">
         <jsp:param name="title" value="Your Profile" />
     </jsp:include>
 </head>
 <body>
+    <h1>Hello Admin</h1>
     <jsp:include page="partials/navbar.jsp" />
 
     <div class="container">
         <h1>Viewing your profile.</h1>
+        <button>logout</button>
     </div>
+
+
 
 </body>
 </html>
